7a86e8fd11f1ddd4a1d6cc6f28ee9c37049f58f3
BillyEsteves <joaopalma.esteves@gmail.com>
2025-04-30 22:04:11 +0100

    'solutionAlgorythm' function implemented, however it is returning the wrong results. Maybe the ordering of the edges by cost in the 'createGraph' function is wrong

--- solucao.cpp
+++ solucao.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <unordered_set>
 
 /*===========[ FUNCTION/GLOBAL VARS DECLARATION ]==========*/
 
+bool CONTINUESEARCH = true;
+
 std::vector<std::vector<std::pair<int, int>>> createGraph(int **edges, int E, int N);
 void depthFirstSearch(int node, std::vector<std::vector<int>>& graph, std::vector<bool>& visited);
 bool mandatoryConnected(int* mandatoryTowers, int M, std::vector<std::vector<int>>& graph);
+int solutionAlgorythm(std::vector<std::vector<std::pair<int, int>>> graph, int currentTower, int edgeCost, bool* visited, std::vector<std::pair<int, bool>> mandatory);
 
 /*===========[ FUNCTIONS ]==========*/
 
@@ -22,8 +26,16 @@ std::vector<std::vector<std::pair<int, int>>> createGraph(int **edges, int E, in
         int cost = edges[i][2];
 
         newGraph[u].push_back({v, cost});
-        newGraph[v].push_back({u, cost});  // since it's undirected
+        newGraph[v].push_back({u, cost});
     }
+
+      // Sort by cost (helps with main solution)
+    for (std::vector<std::pair<int, int>> element : newGraph) {
+        std::sort(element.begin(), element.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+            return a.second < b.second;
+        });
+    }
+
     return newGraph;
 }
 
@@ -49,6 +61,64 @@ bool mandatoryConnected(int* mandatoryTowers, int M, std::vector<std::vector<std
             return false;
     }
     return true;
+}
+
+int solutionAlgorythm(std::vector<std::vector<std::pair<int, int>>> graph, int currentTower, int edgeCost, bool* visited, std::vector<std::pair<int, bool>> mandatory) {
+    /*  
+        1 - For every tower we check a list with all the towers, but also a
+        special list, containing only the mandatory towers, if the current
+        is one of them. 
+
+        2 - If all mandatory towers have been checked, we backtrack the path we
+        have created.
+
+        3- If it is not over yet, we iterate through all the paths available
+        to the current tower, from least to most costly of course.
+        We do not go to towers that have already been visited, to avoid loops.
+        
+        4 - We check the cost of the path we went down through. If the cost
+        returned is greater than the cost of the path we took to get here,
+        we return that value as the currentBiggestEdge. If not, then
+        currentBiggestEdge is the cost of the edge that lead us to our
+        current position.
+    */
+    
+    int currentBiggestEdge = -1;
+
+    if (CONTINUESEARCH) {
+        // 1 - Mark the tower as visited
+        visited[currentTower] = true;
+
+        // 1 - Mark in the mandatory list, if its a mandatory tower
+        for(std::pair<int, bool> tower : mandatory) {
+            if (tower.first == currentTower) {
+                tower.second = true;
+                break;
+            } 
+        }
+
+        // 2 - Check if all mandatory towers have been visited
+        CONTINUESEARCH = false;
+        for(std::pair<int, bool> tower : mandatory) {
+            if (!tower.second) {
+                CONTINUESEARCH = true;
+                break;
+            }
+        }
+        if(!CONTINUESEARCH) return edgeCost; // The cost of the last path
+        
+        // 3 - Continue the search
+        for (std::pair neighbour : graph[currentTower]){    //<--- already sorted by cost
+            if(!visited[neighbour.first]) {
+                // 4 - Check all edges
+                currentBiggestEdge = solutionAlgorythm(graph, neighbour.first, neighbour.second, visited, mandatory);
+                if(edgeCost > currentBiggestEdge) currentBiggestEdge = edgeCost;
+                if(!CONTINUESEARCH) break;
+            }
+        }
+    }
+
+    return currentBiggestEdge;
 }
 
 /*===========[ MAIN ]==========*/
@@ -81,10 +151,18 @@ int main() {
         is not possible to connect all mandatory towers.
     */
 
-    int N, E, M;
+    int N, E, M, maxEdgeValue = 0;
+
 
     while(std::cin >> N >> E) {
+
         int **edges = new int*[E];
+        bool *visitedTowers = new bool[N];
+        std::vector<std::pair<int, bool>> visitedMandatory;
+        for(int i = 0; i < N; ++i) {
+            visitedTowers[i] = false;
+        }
+
         for(int i = 0; i < E; i++) {
             edges[i] = new int[3];
             std::cin >> edges[i][0] >> edges[i][1] >> edges[i][2];
@@ -94,6 +172,7 @@ int main() {
         int *mandatory = new int[M];
         for(int i = 0; i < M; i++) {
             std::cin >> mandatory[i];
+            visitedMandatory.push_back({mandatory[i], false});
         }
 
         /*===========[ PRE PROCESSING ]==========*/
@@ -114,18 +193,49 @@ int main() {
             }
             delete[] edges;
             delete[] mandatory;
+            delete[] visitedTowers;
             continue;
         }
         /*===========[ SOLUTION ]==========*/
-        
+
         /* 
+            João's solution:
+            1. Sort the neighbours of all towers in terms of cost. (Done in createGraph)
+            2. Create the following variables:
+                - bool visitedTowers[N]: to check all visited towers. this is used to avoid cycles.
+                - bool visitedMandatory[M]: to check all visited mandatory towers. once all true, we can stop the search.
+                - bool CONTINUESEARCH: to control the search for mandatory towers. once all mandatory towers are visited, turn to false
+            3. create a recursive function(graph, current_tower, biggestEdgeYet, visitedTowers, mandatoryTowers):
+                currentBiggestEdge = biggestEdgeYet
+                tempEdge = 0
+
+                if CONTINUESEARCH is true && !visitedTowers[current_tower]:
+                    mark it as visited in visitedTowers.
+
+                    if 'current_tower' is mandatory:
+                        mark it as visited in visitedMandatory.
+                    
+                    if all mandatory towers are visited:
+                        CONTINUESEARCH = false;
+                        return currentBiggestEdge
+                        
+                    else:
+                        for (neighbour : graph[current_tower],first):    <--- already sorted by cost
+                            tempEdge = function(graph, neighbour, biggestEdgeYet, visitedTowers, mandatoryTowers)
+                            if tempEdge > currentBiggestEdge: currentBiggestEdge = tempEdge
+                else:
+                    return currentBiggestEdge
+
         // Kruskal's algorithm to find the maximum edge value in the MST
         // of the mandatory towers
-        // ...
+        */
 
         // Print the result
-        // ...
-        */
+        
+
+        maxEdgeValue = solutionAlgorythm(graph, mandatory[0], 0, visitedTowers, visitedMandatory);
+
+        std::cout << maxEdgeValue << "\n";
 
         //Free memory 
         for(int i = 0; i < E; i++) {
@@ -133,6 +243,11 @@ int main() {
         }
         delete[] edges;
         delete[] mandatory;
+        delete[] visitedTowers;
+
+        // Reset variables for next test case
+        maxEdgeValue = 0;
+        CONTINUESEARCH = true;
     }
 
     return 0;
