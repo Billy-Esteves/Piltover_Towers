71c587dade13cfdce1a64515af726ecde7f58280
Sofia Leitão <132916949+sofia-leitao@users.noreply.github.com>
2025-05-09 14:58:06 +0100

    170 points

---
+++ solution.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*===========[ STRUCTURES ]==========*/
+struct unionFind {
+    vector<int> parent;
+
+    // assign parents to the nodes
+    unionFind(int n) : parent(n) {
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    // union of different groups
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        parent[rootX] = rootY;
+    }
+
+    // find the parent of a node
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+};
+
+struct Edge {
+    int origin, destination, cost;
+};
+
+/*===========[ FUNCTIONS ]==========*/
+bool comparator(const Edge& x, const Edge& y) {
+    return x.cost < y.cost;
+}
+
+bool canFormEdges(const vector<Edge>& edges, int maxCost, const vector<int>& mandatory, int x) {
+    // assign a parent to x
+    unionFind u(x);
+
+    // if the cost of the edge is bigger than the max it isn't part of the solution
+    for (const Edge& e : edges) {
+        if (e.cost > maxCost) break;
+        u.unite(e.origin, e.destination);
+    }
+
+    int representative = u.find(mandatory[0]);
+    // check if the connection is possible
+    for (size_t i = 1; i < mandatory.size(); i++) {
+        if (u.find(mandatory[i]) != representative)
+            return false;
+    }
+
+    return true;
+}
+
+int solution(int N, const vector<Edge>& edges, const vector<int>& mandatory) {
+    vector<Edge> sortedEdges = edges;
+    sort(sortedEdges.begin(), sortedEdges.end(), comparator);
+
+    vector<int> cost;
+    for (const Edge& x : sortedEdges) {
+        // list is ordered so we only need to check the last element
+        if (cost.empty() || cost.back() != x.cost) {
+            cost.push_back(x.cost);
+        }
+    }
+
+    // binary search
+    int result = -1;
+    auto low = cost.begin(), high = cost.end() - 1;
+    while (low <= high) {
+        auto mid = low + (high - low) / 2;
+        int maxCost = *mid;
+
+        if (canFormEdges(sortedEdges, maxCost, mandatory, N)) {
+            result = maxCost;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return result;
+}
+
+/*===========[ MAIN ]==========*/
+int main() {
+    int N, E;
+
+    while (cin >> N >> E) {
+        vector<Edge> edges;
+
+        for (int i = 0; i < E; i++) {
+            int origin, destination, cost;
+            cin >> origin >> destination >> cost;
+            edges.push_back({origin, destination, cost});
+        }
+
+        int M;
+        cin >> M;
+        vector<int> mandatory(M);
+        for (int i = 0; i < M; i++) {
+            cin >> mandatory[i];
+        }
+
+        int result = solution(N, edges, mandatory);
+
+        if (result == -1) {
+            cout << "Impossible to connect!" << endl;
+        } else {
+            cout << result << endl;
+        }
+    }
+
+    return 0;
+}
